58fe6fecd28dc9681677835a4df9acd3dc48255e
KarenCKL <113635618+KarenCKL@users.noreply.github.com>
2024-07-06 22:26:17 -0400

    added translation

--- src/components/Facilities.js
+++ src/components/Facilities.js
@@ -2,41 +2,43 @@ import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWifi, faWheelchair, faParking, faRestroom} from '@fortawesome/free-solid-svg-icons';
+import { useTranslation } from 'react-i18next';
 export default function Facilities() {
+    const { t } = useTranslation();
     return (
         <div>
-            <Container className="mt-2 p-4">
+            <Container className="mt-1 p-4">
                 <Row>
                     <div className="d-flex justify-content-center mb-5 text-info fst-italic">
-                        <h3>Our Amenities</h3>
+                        <h3>{t('amenities')}</h3>
                     </div>
                     <Col lg={3} sm={6}>
                         <div className="d-flex justify-content-center">
                             <FontAwesomeIcon icon={faParking} size='3x' className="mb-3 mt-2" style={{ color: '#a60341' }} />
                         </div>
-                        <h5 className='text-center'>Free Parking</h5>
-                        <p className='text-center Font'>We offer free parking for all guests. Our parking lot is secure and well-lit.</p>
+                        <h5 className='text-center'>{t('parking')}</h5>
+                        <p className='text-center Font'>{t('parking1')}</p>
                     </Col>
                     <Col lg={3} sm={6}>
                         <div className="d-flex justify-content-center">
                             <FontAwesomeIcon icon={faWheelchair} size='3x' className="mb-3 mt-2" style={{ color: '#a60341' }} />
                         </div>
-                        <h5 className='text-center'>Wheelchair Accessible</h5>
-                        <p className='text-center Font'> Our club is wheelchair accessible to ensure convenience for all guests.</p>
+                        <h5 className='text-center'>{t('wheelchair')}</h5>
+                        <p className='text-center Font'>{t('wheelchair1')}</p>
                     </Col>
                     <Col lg={3} sm={6}>
                         <div className="d-flex justify-content-center">
                             <FontAwesomeIcon icon={faWifi} size='3x' className="mb-3 mt-2" style={{ color: '#a60341' }} />
                         </div>
-                        <h5 className='text-center'>Free Wifi</h5>
-                        <p className='text-center Font'>We offer free high speed internet to all guests.</p>
+                        <h5 className='text-center'>{t('wifi')}</h5>
+                        <p className='text-center Font'>{t('wifi1')}</p>
                     </Col>
                     <Col lg={3} sm={6}>
                         <div className="d-flex justify-content-center">
                             <FontAwesomeIcon icon={faRestroom} size='3x' className="mb-3 mt-2" style={{ color: '#a60341' }} />
                         </div>
-                        <h5 className='text-center'>Changing Room</h5>
-                        <p className='text-center Font'>We offer changing rooms for guests to freshen up after a game.</p>
+                        <h5 className='text-center'>{t('changing')}</h5>
+                        <p className='text-center Font'>{t('changing1')}</p>
                     </Col>
                 </Row>
             </Container>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,25 +4,27 @@ import instagram from '../assets/instagram.svg';
 import linkedin from '../assets/linkedin.svg';
 import twitterX from '../assets/twitter-x.svg';
 import youtube from '../assets/youtube.svg';
+import { useTranslation } from 'react-i18next';
 
 const Footer = () => {
+    const { t } = useTranslation();
     return (
         <footer className="bg-primary mt-4 pt-4 pb-3">
             <Container>
                 <Row>
                     <Col>
-                        <p className='fs-5 text-white'>&copy; 2024 Ottawa Badminton Club. All rights reserved.</p>
+                        <p className='fs-5 text-white'>{t('copyright')}</p>
                     </Col>
                     <Col>
                         <div style={{ paddingLeft: "100px" }}>
-                            <p className='text-white'>Email: <a href="mailto:info@badmintonClub.ca" className="text-white" id='emailFooter'>info@badmintonClub.ca</a></p>
-                            <p className='text-white'>Tel: <a href="+123456789" className="text-white" id='phoneFooter'>+1 (234) 567-890</a>
+                            <p className='text-white'>{t('email')} <a href="mailto:info@badmintonClub.ca" className="text-white" id='emailFooter'>info@badmintonClub.ca</a></p>
+                            <p className='text-white'>{t('tel')}<a href="+123456789" className="text-white" id='phoneFooter'>+1 (234) 567-890</a>
                             </p>
                         </div>
                     </Col>
                     <Col className="d-flex flex-column">
                         <div style={{ paddingLeft: "100px" }}>
-                            <p className='fs-5 mb-0 text-white'>Follow us on:</p>
+                            <p className='fs-5 mb-0 text-white'>{t('follow')}</p>
                         </div>
                         <div className='mt-2' style={{ paddingLeft: "120px" }}>
                             <img src={facebook} alt="facebook icon" className="img-fluid ms-3" style={{ width: "25px", height: "25px", cursor: "pointer" }} href="" />
